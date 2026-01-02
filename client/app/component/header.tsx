7a1124e07c71058447c55c73f91d7575bbc69b7b
Guytanthong <tanthongguy292@gmail.com>
2026-01-02 15:08:50 +0700

    rightdone

--- client/app/component/header.tsx
+++ client/app/component/header.tsx
@@ -5,6 +5,9 @@ import { useState } from "react";
 
 export default function Header() {
   const [activeTab, setActiveTab] = useState("");
+  const [activeRight, setActiveRight] = useState("");
+
+  
   return (
     <header className="grid grid-cols-[320px_1fr_320px] items-center bg-[#1C1E21] h-14 px-4 border-b border-[#3A3B3C]">
         <div className="flex items-center h-10 px-[0.5px] gap-2 ">
@@ -67,9 +70,83 @@ export default function Header() {
         
 
             
-          
+        <div className="flex items-center gap-2 pl-[125px]">
+
+          {/* Menu */}
+          <div
+            onClick={() => setActiveRight("menu")}
+            className={`w-10 h-10 rounded-full flex items-center justify-center cursor-pointer
+              ${activeRight === "menu" ? "bg-[#0866FF] "  : "bg-[#3A3B3C] hover:bg-[#4E4F50]"}
+            `}
+          >
+            <Image
+              src="/icons/rightcontent/menu.svg"
+              width={22}
+              height={22}
+              alt="menu"
+              className={`${activeRight === "menu" ? "invert" : ""}` }
+            />
+          </div>
+
+          {/* Messenger */}
+          <div
+            onClick={() => setActiveRight("messenger")}
+            className={`relative w-10 h-10 rounded-full flex items-center justify-center cursor-pointer
+              ${activeRight === "messenger" ? "bg-[#0866FF]" : "bg-[#3A3B3C] hover:bg-[#4E4F50]"}
+            `}
+          >
+            <Image
+              src="/icons/rightcontent/messenger.svg"
+              width={22}
+              height={22}
+              alt="messenger"
+              className={`${activeRight === "messenger" ? "invert" : ""}`}
+            />
+
+            <span className="absolute -top-1 -right-1 bg-[#e41e3f] text-white text-[10px] px-[6px] py-[1px] rounded-full">
+              3
+            </span>
+          </div>
+
+          {/* Notification */}
+          <div
+            onClick={() => setActiveRight("notification")}
+            className={`relative w-10 h-10 rounded-full flex items-center justify-center cursor-pointer
+              ${activeRight === "notification" ? "bg-[#0866FF]" : "bg-[#3A3B3C] hover:bg-[#4E4F50]"}
+            `}
+          >
+            <Image
+              src="/icons/rightcontent/notification.svg"
+              width={22}
+              height={22}
+              alt="notification"
+              className={`${activeRight === "notification" ? "invert" : ""}`}
+            />
+
+            <span className="absolute -top-1 -right-1 bg-[#e41e3f] text-white text-[10px] px-[6px] py-[1px] rounded-full">
+              16
+            </span>
+          </div>
+
+          {/* Profile */}
+          <div
+            onClick={() => setActiveRight("profile")}
+            className={`w-10 h-10 rounded-full flex items-center justify-center cursor-pointer
+              ${activeRight === "profile" ? "bg-[#0866FF]" : "bg-[#3A3B3C] hover:bg-[#4E4F50]"}
+            `}
+          >
+            <Image
+              src="/icons/rightcontent/profile.svg"
+              width={22}
+              height={22}
+              alt="profile"
+              className={`${activeRight === "profile" ? "invert" : ""}`}
+            />
+          </div>
+
+        </div>
+                
         
-        <div>Right content</div>
     </header>
      
   );
