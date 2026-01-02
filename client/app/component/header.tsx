b6276aef1d7707060d9bb7c59a474e4511f1d684
Guytanthong <tanthongguy292@gmail.com>
2026-01-02 14:42:56 +0700

    center done

--- client/app/component/header.tsx
+++ client/app/component/header.tsx
@@ -6,15 +6,16 @@ import { useState } from "react";
 export default function Header() {
   const [activeTab, setActiveTab] = useState("");
   return (
-    <header className="flex justify-between items-center bg-[#1C1E21] h-14 px-4 shadow border-b border-b-[0.5px] border-[#3A3B3C]">
+    <header className="grid grid-cols-[320px_1fr_320px] items-center bg-[#1C1E21] h-14 px-4 border-b border-[#3A3B3C]">
         <div className="flex items-center h-10 px-[0.5px] gap-2 ">
+            
             <div className=" flex items-center justify-center text-white text-2xl font-bold">
               <Image 
                 src="/icons/fb.png"
                 alt="logo icon"
                 width={40} //bg-blue-500
                 height={40} //w-10 h-10 rounded-full 
-                className="opacity-80"
+                className="opacity-100"
               />
             </div>
             
@@ -31,36 +32,43 @@ export default function Header() {
             <input placeholder="Search Facebook" className="font-sans   text-[15px] outline-none text-[#858585] placeholder-[#B0B3B8]  " />
             
             </div>
-            
-
-            
         </div>
 
-        <div className="flex flex-1 justify-center">
-          <div className="flex gap-6">
-            <Image
-              src={activeTab === "home"
-                ? "/icons/homeblue.svg"
-                : "/icons/homegrey.svg"}
-              width={24}
-              height={24}
-              alt="home"
-              onClick={() => setActiveTab("home")}
-              className="cursor-pointer"
-            />
 
-            <Image
-              src={activeTab === "friend"
-                ? "/icons/friendblue.svg"
-                : "/icons/friendgrey.svg"}
-              width={24}
-              height={24}
-              alt="home"
-              onClick={() => setActiveTab("friend")}
-              className="cursor-pointer"
-            />
+
+        <div className="flex  justify-center">
+          <div className="flex justify-between items-center w-[680px]">
+            {["home","friend","reels","marketplace","gaming"].map(tab => (
+              <div 
+                key={tab}
+                className="relative h-14 flex flex-1 justify-center items-center cursor-pointer"
+                onClick={() => setActiveTab(tab)}
+              >
+                
+                <Image
+                  src={
+                    activeTab === tab
+                      ? `/icons/${tab}blue.svg`
+                      : `/icons/${tab}grey.svg`
+                  }
+                  width={24}
+                  height={24}
+                  alt={tab}
+                />
+
+                {/* Blue underline */}
+                {activeTab === tab && (
+                  <div className="absolute bottom-0 h-[3px] w-full bg-[#0866FF] rounded-full" />
+                )}
+              </div>
+            ))}
           </div>
         </div>
+        
+
+            
+          
+        
         <div>Right content</div>
     </header>
      
